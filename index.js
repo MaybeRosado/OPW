a5e64abea8eb3dcc19713a1bedf589e936213528
MaybeRosado <erosado@ucol.mx>
2025-01-13 09:28:08 -0600

    Connecting to MongoDB

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
+require('dotenv').config()
 const cors = require('cors')
 const express = require('express');
 const app = express();
+const Person = require('./models/person');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'))
 
-app.post('/api/persons', (req, res) => {
-    res.status(201).json({message: 'Person added', data: req.body})
-})
+// app.post('/api/persons', (req, res) => {
+//     res.status(201).json({message: 'Person added', data: req.body})
+// })
 
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/info', (request, response) => {
@@ -47,14 +28,9 @@ app.get('/api/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-
-    if(person){
-        response.json(person);
-    }else{
-        response.status(404).end();
-    }
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -65,40 +41,26 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
-const generateId = () => {
-    const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-    return maxId + 1
-}
-
 app.post('/api/persons', (request, response) => {
     const body = request.body;
     
-    const duplicate = persons.find(person => person.name === body.name);
-
-    if(!body.name || !body.number){
+    if(body.name === undefined){
         return response.status(400).json({
-            error: 'Information missing'
-        })
-    }else if(duplicate){
-        return response.status(400).json({
-            error: 'Name must be unique' 
+            error: 'content missing'
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person);
-    console.log(person)
-    response.json(person);
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 })
